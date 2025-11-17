5f4a2229dcddab53b1fa93a3cab02d6768ddfb1a
lxchurbakov <lxchurbakov@gmail.com>
2025-11-17 20:57:54 +0300

    hehehehe

--- src/editor/jsonx.ts
+++ src/editor/jsonx.ts
@@ -1,3 +1,6 @@
+import React from 'react';
+import get from 'lodash/get';
+
 const is_exec = (value) => {
     return Array.isArray(value) && (typeof value[0] === 'string') && value[0].startsWith('@');
 };
@@ -7,8 +10,9 @@ const flatten = (...css) =>
 
 const exec = (refs, [predicate, ...args]) => {
     const core = {
-        '@': (name) => refs[name],
+        '@': (name) => get(refs, name),
         '@!': (name) => name,
+        '@!!': (value) => (...args) => process({ ...refs, ...args }, value),
         // '@x': (name, ...args) => process({ ...refs, ...process(refs, args) }, refs[name]),
         '@@': (extension, expression) => process({ ...refs, ...process(refs, extension) }, expression),
 
@@ -27,13 +31,46 @@ const exec = (refs, [predicate, ...args]) => {
         '@-': (a, b) => process(refs, a) - process(refs, b),
         '@+': (a, b) => process(refs, a) + process(refs, b),
 
+        // For strings
+        '@.': (a, b) => process(refs, a) + process(refs, b),
+
         // Objects
         // '@at'
         // '@merge': (a, b) => ({ ...process(refs, a), ...process(refs, b) }),
+        // JSX
+        '@jsx': (tag, props, children) => React.createElement(process(refs, tag), process(refs, props), process(refs, children)),
+
+        // Reactive-ness
+        '@state': (scope, value) => {
+            let defaultState = process(refs, scope);
+
+            const Component = () => {
+                const [state, setState] = React.useState(defaultState);
+
+                return process({ ...refs, ...state, set: (refs, wtf) => setState(process({ ...refs, ...state }, wtf)) }, value);
+            };
+
+            return React.createElement(Component);
+
+            // return {
+            //     __state: state,
+            //     value: () => process({ ...refs, ...state, })
+            //     // __stateful: true,
+            //     // state: process(refs, scope),
+            //     // value: process({ ...refs, ...process(refs, scope) }, value)
+            // };
+        },
     };
 
     for (let key in refs) {
+        if (typeof refs[key] === 'function') {
+            core['@' + key] = (...args) => refs[key](refs, ...args);
+        }
+
         if (is_exec(refs[key])) {
+            // if (typeof core['@' + key] === 'function') {
+            //     core['@' + key] 
+            // }
             core['@' + key] = (...args) => process({ ...refs, ...process(refs, args) }, refs[key]);
         }
     }
--- src/editor/page.tsx
+++ src/editor/page.tsx
@@ -6,12 +6,69 @@ import { Base, Flex, Container, Text } from 'lib/atoms';
 import { Monaco } from '../components/monaco';
 import { process } from './jsonx';
 
+interface Props {
+  children: React.ReactNode;
+  fallback?: React.ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback || <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+/* Example 
+
+["@state", {
+    "value": "wtf"
+}, [
+    ["@jsx", "div", {}, [
+        ["@jsx", "input", {
+            "value": ["@", "value"], 
+            "onChange": ["@!!", 
+                ["@set", { "value": ["@", "0.target.value"] }]
+            ]
+        }],
+        ["@jsx", "div", {}, 
+            ["@.", "value is ", ["@", "value"]]
+        ]
+    ]]
+]]
+
+*/
+
 export const EditorPage = () => {
     const [code, setCode] = React.useState('');
+
     const result = React.useMemo(() => {
         try {
             const result = process({}, JSON.parse(code));
-            return JSON.stringify(result);
+
+            // console.log({result})
+// 
+            // return null;
+
+            return result;
         } catch (e) {
             return `cant parse: ${e.toString()}`;
         }
@@ -19,7 +76,7 @@ export const EditorPage = () => {
 
     return (
         <Container p="128px 0">
-            <Text size="32px" weight="bold" mb="48px">Editor Page</Text>
+            <Text size="32px" weight="bold" mb="48px">React with JSONx</Text>
 
             <Flex h="60vh" gap="12px">
                 <Monaco w="100%" h="100%" border="1px solid white" value={code} onChange={($) => setCode($)} />
@@ -27,12 +84,13 @@ export const EditorPage = () => {
                 <Base w="100%" h="100%">
                     <Text>
                         <code>
-                            {result}
+                            <ErrorBoundary>
+                                {result}
+                            </ErrorBoundary>
                         </code>
                     </Text>
                 </Base>
             </Flex>
-            
         </Container>
     );
 };
